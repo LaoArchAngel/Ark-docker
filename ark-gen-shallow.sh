6c9146ab04abd2faa98ec99f17a59d358951dea6
Lao <lao@nexverse.net>
2020-08-15 22:32:59 -0600

    Fix bad quotes

--- ark-gen-shallow.sh
+++ ark-gen-shallow.sh
@@ -4,7 +4,7 @@ instPath=$1
 
 [[ -z "$instPath" ]] && exit 1
 
-echo "Generating shallow instance @ "$instPath""
+echo "Generating shallow instance @ $instPath"
 [[ -d "$instPath" ]] &&  rm -Rf "$instPath"
 
 cp -Ras /ark/server/install "$instPath"
