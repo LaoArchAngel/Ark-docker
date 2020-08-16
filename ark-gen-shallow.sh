e0f2e94b4e71e934e479407fef5f54fba4325387
Lao <lao@nexverse.net>
2020-08-15 22:32:16 -0600

    Shallow arks now support shared mod folder

--- ark-gen-shallow.sh
+++ ark-gen-shallow.sh
@@ -8,3 +8,8 @@ echo "Generating shallow instance @ "$instPath""
 [[ -d "$instPath" ]] &&  rm -Rf "$instPath"
 
 cp -Ras /ark/server/install "$instPath"
+
+modPath="${instPath}/ShooterGame/Content/Mods"
+rm -Rf "$modPath"
+# Mods need to be done as a folder instead of per mod.
+ln -s /ark/server/install/ShooterGame/Content/Mods "$modPath"
