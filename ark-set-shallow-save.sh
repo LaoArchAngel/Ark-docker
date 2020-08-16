dad2bb7212d7f3fa3e53380e2aa8ec1569d4f035
Lao <lao@nexverse.net>
2020-08-15 19:10:21 -0600

    Script to create/link a save path to a shallow instance

---
+++ ark-set-shallow-save.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+instPath=$1
+saveName=$2
+
+[[ -z "$instPath" ]] $$ exit 1
+[[ -z "$saveName" ]] $$ exit 2
+
+fullSave="/ark/saves/"$2""
+targetSave="${instPath}/ShooterGame/Saved"
+
+[[ ! -d "$fullSave" ]] && mkdir "$fullSave"
+
+[[ -d "$targetSave" ]] && rm -Rf "$targetSave"
+ln -s "$fullSave" "$targetSave"
