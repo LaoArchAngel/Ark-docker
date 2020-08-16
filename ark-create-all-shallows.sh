b86be7b630904fc2ce242791196e46944d634455
Lao <lao@nexverse.net>
2020-08-16 03:18:23 -0700

    Wildcard matching for main.cfg

--- ark-create-all-shallows.sh
+++ ark-create-all-shallows.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 for cfg in /ark/config/instances/*.cfg; do
-    [[ "$cfg" == "main.cfg" ]] && continue
+    [[ "$cfg" == *"/main.cfg" ]] && continue
 
     /usr/local/bin/ark-create-shallow "$cfg"
 done
