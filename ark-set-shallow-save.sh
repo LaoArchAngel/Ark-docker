9492e982f4a2014e90272d73b4aff071cf288fbc
Lao <lao@nexverse.net>
2020-08-17 16:31:59 -0600

    Various script cleanups

--- ark-set-shallow-save.sh
+++ ark-set-shallow-save.sh
@@ -6,13 +6,13 @@ saveName=$2
 [[ -z "$instPath" ]] && exit 1
 [[ -z "$saveName" ]] && exit 2
 
-fullSave="/ark/saves/"$2""
+fullSave="/ark/saves/$2"
 targetSave="${instPath}/ShooterGame/Saved"
 
-echo "Linking save path "$fullSave" to shallow instance "$targetSave""
+echo "Linking save path $fullSave to shallow instance $targetSave"
 
 mkdir -p "$fullSave"
 
 rm -Rf "$targetSave"
 ln -sf "$fullSave" "$targetSave"
-[[ "$saveName" -ne "main" ]] && ln -sf /ark/saves/main/clusters "${targetSave}/clusters"
+[ "$saveName" -ne "main" ] && ln -sf /ark/saves/main/clusters "${targetSave}/clusters"
